2b1c3c5b24197e86ff0608a70ee8c8414b7a1dd3
Arash <arashsaleh798@gmail.com>
2025-02-24 09:42:24 -0800

    made middleware and controllers posts for auth

--- models/user.js
+++ models/user.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
+const golfSchema = new mongoose.Schema({
+  course: {
+    type: String, 
+    required: true,
+  }, 
+  score: {
+    type: Number,
+    required: true, 
+  },
+  holes: {
+    type: Number
+  },
+  notes: {
+    type: String,
+  },
+  difficulty: {
+    type: String, 
+    enum: ['easy', 'medium', 'hard', 'never again'],
+  },
+})
+
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,7 +31,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  golf: [golfSchema],
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
